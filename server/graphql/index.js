ebac43f9c4a8df5768738840a517c2ff018af3ca
Âgte <agte@iMac-de-Agte.local>
2016-11-01 18:23:45 +0100

    Clean code

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,25 +1,12 @@
 import {
     GraphQLSchema,
-    GraphQLObjectType,
-    GraphQLBoolean
+    GraphQLObjectType
 } from 'graphql';
 
 import me from './queries/me';
 import all from './queries/all';
 
 import addUser from './mutations/user';
-//const schema = new Schema({
-//    query: new ObjectType({
-//        name: 'Query',
-//        fields: {
-//            me
-//        },
-//    }),
-//});
-//
-//export default schema;
-
-
 
 const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
